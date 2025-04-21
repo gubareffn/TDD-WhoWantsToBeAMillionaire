305b33b2c7b3071baac4d014cd83af784bff7204
gubar <gubarev2503@mail.ru>
2025-04-22 03:26:51 +0700

    Реализация призов

--- src/com/millionaire/Game.java
+++ src/com/millionaire/Game.java
@@ -4,6 +4,11 @@ public class Game {
     private final Question[] questions;
     private int currentLevel;
     private Question currentQuestion;
+    private static final int[] PRIZES = {
+            500, 1000, 2000, 5000, 10000,
+            25000, 50000, 100000, 250000,
+            500000, 750000, 1000000
+    };
 
     public Game(Question[] questions) {
         this.questions = questions;
@@ -16,6 +21,9 @@ public class Game {
         if (currentLevel < questions.length) {
             currentQuestion = questions[currentLevel];
         }
+    }
+    public int getPrize(int level) {
+        return PRIZES[Math.min(level, PRIZES.length - 1)];
     }
 
     public boolean isCompleted() {
